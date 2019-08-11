9c06db8691203cbfa5a2250c76ac180286735b92
chao <joseph.chaozhang@gmail.com>
2019-08-11 17:11:03 -0400

    Added cookie service to UI and env properties to api service

--- restapi/src/main/java/com/upnxtblog/samples/controller/RestAPIController.java
+++ restapi/src/main/java/com/upnxtblog/samples/controller/RestAPIController.java
@@ -3,14 +3,18 @@ package com.upnxtblog.samples.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.upnxtblog.samples.dao.CustomerRepository;
 import com.upnxtblog.samples.model.Country;
+import com.upnxtblog.samples.model.Customer;
 
 @RestController
 public class RestAPIController {
@@ -18,13 +22,20 @@ public class RestAPIController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@RequestMapping("/api/hello")
+	@Autowired
+    CustomerRepository customerRepository;
+	
+	@Value("${environment}")
+	private String env;
+	
+	
+	@GetMapping("/api/hello")
 	public String hello() {
 		return "Hello world! >>> <a href='http://upnxtblog.com' target='_blank'>upnxtblog.com</a>";
 	}
 	
 	// https://restcountries.eu/#api-endpoints-all
-	@RequestMapping("/api/countries")
+	@GetMapping("/api/countries")
 	public List<Country> getContries() {
 		String URL = "https://restcountries.eu/rest/v2/all";
 		ResponseEntity<List<Country>> responseEntity = 
@@ -32,5 +43,15 @@ public class RestAPIController {
 		List<Country> pojoObjList = responseEntity.getBody();
 		
 		return pojoObjList;
+	}
+	
+	@GetMapping("/api/customers")
+	public Iterable<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+	
+	@GetMapping("/api/env")
+	public String getEnv() {
+		return this.env;
 	}
 }
--- restapi/src/main/webapp/angular6-client/src/app/app.component.ts
+++ restapi/src/main/webapp/angular6-client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Upnxtblog Sample : This is Angular 6 Application';
+
+  constructor( private cookieService: CookieService ) {}
+
+  ngOnInit(): void {
+    this.cookieService.set( 'appCookie', 'This is hello apps.' );
+    const allCookies = this.cookieService.getAll();
+    console.log(allCookies);
+  }
 }
