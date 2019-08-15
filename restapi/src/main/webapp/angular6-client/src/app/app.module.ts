eba2171a4f4c8da2b0f3098c91aade15debcae66
chao <joseph.chaozhang@gmail.com>
2019-08-14 22:08:58 -0400

    Added Resolver example

--- restapi/src/main/webapp/angular6-client/src/app/app.module.ts
+++ restapi/src/main/webapp/angular6-client/src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
 import { CookieService } from 'ngx-cookie-service';
+import { UsersListService } from './service/users-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [ CookieService ],
+  providers: [ CookieService, UsersListService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
